cad1850cd171cc4e4e9df1b935c9965507c922b4
alaa-rish <alaa.rish2@gmail.com>
2021-06-20 21:21:25 +0300

    new files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let userName = prompt ("what is your name?");
+console.log(userName);
+alert("Welcome " + userName);
+document.write("<p style='color:black;'> What is Your Name?  " + userName + " </p>");
+
+let from = prompt ("Where are you from?");
+console.log(from);
+document.write("<p style='color:black;'> Where are you from?  " + from + " </p>");
+
+let dest = prompt ("Where are you going to go?");
+console.log(dest);
+document.write("<p style='color:black;'> Where are you going to go?  " + dest + " </p>");
+
+let hotel = prompt ("how many stars of your chosed hotel?");
+console.log(hotel);
+document.write("<p style='color:black; '> Where are you going to go?  " + hotel + " </p>");
+
+alert("check the end of the page");
+
+
