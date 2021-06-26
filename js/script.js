eb152103afcb34a80ee15f08fbc041654a64b191
alaa-rish <alaa.rish2@gmail.com>
2021-06-26 21:33:38 +0300

    fix HTMl & add Java Alerts

--- js/script.js
+++ js/script.js
@@ -5,14 +5,17 @@ document.write("<p style='color:black;'> What is Your Name?  " + userName + " </
 
 let from = prompt ("Where are you from?");
 console.log(from);
+alert(from + ', its a beautiful country');
 document.write("<p style='color:black;'> Where are you from?  " + from + " </p>");
 
 let dest = prompt ("Where are you going to go?");
 console.log(dest);
+alert('Welcome to ' + dest);
 document.write("<p style='color:black;'> Where are you going to go?  " + dest + " </p>");
 
 let hotel = prompt ("how many stars of your chosed hotel?");
 console.log(hotel);
+alert(hotel + ' is a good choice');
 document.write("<p style='color:black; '> Where are you going to go?  " + hotel + " </p>");
 
 alert("check the end of the page");
